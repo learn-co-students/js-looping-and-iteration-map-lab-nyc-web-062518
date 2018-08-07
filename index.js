78d7322031c46f93c682dc94e7d956828cd7fbfc
seanraad123 <seanconrad123@gmail.com>
2018-08-07 15:30:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file.
+
+const drivers = ['Bobby Smith', 'Sammy Watkins', 'Sally Jenkins', 'Annette Sawyer', 'Sarah Hucklebee', 'bobby ridge'];
+
+
+function lowerCaseDrivers(array) {
+  const lowercasedWords = array.map(name => name.toLowerCase());
+  return lowercasedWords
+}
+
+
+function nameToAttributes(array) {
+    new_arr = []
+    array.map(driver => {
+    const splitNames = driver.split(' ')
+    const driverFirst = splitNames[0]
+    const driverLast = splitNames[1]
+    new_arr.push({ firstName: driverFirst, lastName: driverLast })
+  })
+  return new_arr
+}
+
+
+// "<NAME OF DRIVER> is from <HOMETOWN>"
+function attributesToPhrase(array){
+  return array.map(driver => {return`${driver.name} is from ${driver.hometown}`})
+}
